3c94bdc5e0287d6affda06e1f3aab3a828233cee
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-09-17 13:56:28 +0200

    initial commit

---
+++ R/rWSBIM2122.R
@@ -0,0 +1,10 @@
+#' @importFrom utils packageVersion
+
+#' @title Package version
+#'
+#' @export
+#' @examples
+#' ## check the package version that is currently installed
+#' rWSBIM2122version()
+rWSBIM2122version <- function()
+    utils::packageVersion("rWSBIM2122")
