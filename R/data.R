f19a93456769a8949b9fa535fa10a8893ba75350
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-09-20 08:13:11 +0200

    new gexp data, example for lm chapter

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Single Gene Expression Data
+#'
+#' A simple gene expression data in two groups. (Based on the `sleep`
+#' data).
+#'
+#' @md
+#'
+#' @rdname gexp
+#'
+#' @aliases gexp
+#'
+#' @examples
+#' data(gexp)
+#' gexp
+"gexp"
+
