1ec04e639121abc365d9a5acc5f4430f7935aaaf
Laurent Gatto <laurent.gatto@uclouvain.be>
2020-09-23 18:28:36 +0200

    add lmdata for final linmod exercise

--- R/data.R
+++ R/data.R
@@ -14,3 +14,18 @@
 #' gexp
 "gexp"
 
+
+#' Gene expression data
+#'
+#' Factorial design (2 factors with 2 levels each) with gene expression for 5
+#' genes and 12 samples.
+#'
+#' @rdname lmdata
+#'
+#' @aliases lmdata lmannot
+#'
+#' @examples
+#' data(lmdata, verbose = TRUE)
+#' lmdata
+#' lmannot
+"lmdata"
